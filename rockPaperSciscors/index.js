d2ba5330b12e1d02bb3b2cf2cd498b4cde3598ab
VasilisKan <vasiliskan0gmail.com>
2024-06-21 00:53:57 +0300

    Rock paper sciscors establich. --Gets 2 selections. (need to set the winner)

--- rockPaperSciscors/index.js
+++ rockPaperSciscors/index.js
@@ -1,1 +1,21 @@
-console.log("Hello There")
+function playRound(humanchoice, computerChoice){
+
+}
+
+
+const humanSelection = gethumanChoice();
+const computerSelection = getcomputerSelection();
+
+playRound(humanSelection, computerSelection);
+
+function gethumanChoice(){
+    let userChoice = prompt("rock, paper or sciscors?");
+    return userChoice;
+}
+function getcomputerSelection(){
+    const randomNumber = Math.floor(Math.random()*3)+ 1;
+    if (randomNumber === 1 ){ return "sciscors"
+    } else if (randomNumber === 2){ return "paper"
+    } else { return "rock"}
+}
+console.log(computerSelection)
